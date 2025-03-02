6cd9c1b6555134f8feded5c52afa8a47145acbed
Zunair Ali <ranazunairali007@gmail.com>
2025-03-02 21:49:11 +0500

    Admin Login

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,15 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Sidebar from './components/Sidebar'
+import {Routes,Route} from "react-router-dom"
+import Add from './pages/Add'
+import Orders from './pages/Orders'
+import List from './pages/List'
+import Login from './components/Login'
+import {ToastContainer} from "react-toastify"
+
+export const backendUrl= import.meta.env.VITE_BACKEND_URL
 
 function App() {
+    
+    const [token,setToken]= useState(localStorage.getItem("token")?localStorage.getItem("token"):"")
+
+    // creating the local storage sio that when the page loads it will automatically have the access of the token.
+    useEffect(()=>{
+      localStorage.setItem("token",token)
+    },[token])
+
+
   return (
     <div className='bg-gray-50 min-h-screen'>
-      <Navbar/>
+      <ToastContainer/>
+      {token=== ""? <Login setToken={setToken }/>
+      :
+      <>
+      <Navbar setToken={setToken}/>
       <hr />
+      
       <div className='flex-w-full'>
           <Sidebar/>
+          <div className='w-[70%] mx-auto ml-[max(5vw,25vw)] my-8 text-gray-600 text-base'>
+           <Routes >
+             <Route path='/add' element={<Add token={token}/>}/>
+             <Route path='/list' element={<List token={token}/>}/>
+             <Route path='/orders' element={<Orders token={token}/>}/>
+           </Routes>
+
+          </div>
       </div>
+      </>
+      }
+      
     </div>
   )
 }
--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {assets} from "../assets/assets.js"
 
-function Navbar() {
+function Navbar({setToken}) {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <img src={assets.logo} className='w-[80px]' alt="" />
-        <button className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-xl'>Logout</button>
+        <button onClick={()=>setToken("")} className='bg-gray-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-xl'>Logout</button>
     </div>
   )
 }
-
+ 
 export default Navbar
