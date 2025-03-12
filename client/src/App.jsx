f7b1c033fb7400c3aeb2f62757f80ef0afddc9d7
Zunair Ali <ranazunairali007@gmail.com>
2025-03-12 15:41:55 +0500

    Fixed build configuration

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,7 +11,7 @@ import Order from "./pages/Order"
 import Navbar from "./components/Navbar.jsx"
 import Searchbar from "./components/Searchbar.jsx"
 import Footer from "./components/Footer.jsx"
-import { ToastContainer, toast } from 'react-toastify';
+import { ToastContainer} from 'react-toastify';
 import Verify from "./pages/Verify.jsx"
 
 function App() {
--- client/vite.config.js
+++ client/vite.config.js
@@ -4,5 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {port:5173}
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'react-vendor': ['react', 'react-dom', 'react-router-dom'],
+        }
+      }
+    },
+    chunkSizeWarningLimit: 1600
+  },
+  server: {
+    port: 5173
+  },
+  preview: {
+    port: 5173
+  }
 })
